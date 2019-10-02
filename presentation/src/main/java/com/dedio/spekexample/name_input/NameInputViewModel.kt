9c03d95420e9db2a1a4623e2abded544ec6206ea
Pawel Dedio <paweldedio@gmail.com>
2019-10-02 18:30:55 +0200

    Basic view model implementation

--- presentation/src/main/java/com/dedio/spekexample/name_input/NameInputViewModel.kt
+++ presentation/src/main/java/com/dedio/spekexample/name_input/NameInputViewModel.kt
@@ -3,8 +3,15 @@ package com.dedio.spekexample.name_input
 import com.dedio.spekexample.MainApplication
 import com.dedio.spekexample.base.BaseViewModel
 import com.dedio.spekexample.util.ResourceRepository
+import com.dedio.spekexample.util.delegators.MutableLiveDataProvider
 import javax.inject.Inject
 
 class NameInputViewModel @Inject constructor(application: MainApplication, resourceRepository: ResourceRepository) :
-    BaseViewModel(application, resourceRepository){
+    BaseViewModel(application, resourceRepository) {
+
+    val userName by MutableLiveDataProvider<String>()
+
+    fun onSearchClick() {
+
+    }
 }
